dfded91f1c96990692f7041c489eec325918a8c8
Gauravdev28 <gauravagldeveloper28@gmail.com>
2026-06-04 11:32:24 +0530

    Learning File I/O Concept

--- FILEIO/fileio.py
+++ FILEIO/fileio.py
@@ -1,1 +1,13 @@
 # Here we learn how to store data permanently in System by using file handling 
+f = open("/Users/gauravagarwal/Documents/GitHub/python_programs/FILEIO/demo.txt" , "r")
+# data = f.read()
+print(f.read()) # If we read a file it didn't read file again it print blank line 
+#print(f.readline()) # It read only single line of our data file
+
+f.close()
+
+
+# For Writing file 
+h = open("write.txt" , "w+")
+h.write("My Name is Gaurav Agarwal")
+h.close
