eba3f6a88aea933fad86900cfdc1b7a0072cc8e3
Gauravdev28 <gauravagldeveloper28@gmail.com>
2026-06-06 21:28:32 +0530

    Swap two value question

---
+++ Practise/swaptwo.py
@@ -0,0 +1,9 @@
+# WAP to swap two value without making third variable 
+
+a = int(input("Enter Your A Value :"))
+b = int(input("Enter Your B Value :"))
+
+a,b = (b,a)
+
+print("Value of Swap A : ",a )
+print("Value of Swap B : ",b )
