b5911ab56e86cc80d178a22717203758de1fcccd
Gauravdev28 <gauravagldeveloper28@gmail.com>
2026-06-06 21:37:32 +0530

    Finding Area and Parameter of Rectangle

---
+++ Practise/area_perimeter_rectange.py
@@ -0,0 +1,10 @@
+# WAP to find area and parameter of rectangle 
+
+l = int(input("Enter the Length :"))
+w = int(input("Enter the width :"))
+
+area = (l*w)
+parameter = (2*(l+w))
+
+print("Area of Rectangle :" , area)
+print("Parameter of Rectangle :" , parameter)
