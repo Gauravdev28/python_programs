b51d5bad43740250c2b960d54fd4466bea3374fd
Gauravdev28 <gauravagldeveloper28@gmail.com>
2026-06-06 21:19:06 +0530

    Practise Python Question

---
+++ Practise/finding.type.py
@@ -0,0 +1,9 @@
+# WAP to find dara type which kind of value it is 
+int = 5
+print(type(int))
+
+char= "Gaurav"
+print(type(char))
+
+float = 4.66
+print(type(float))
---
+++ Practise/inputage.height.py
@@ -0,0 +1,9 @@
+# WAP to take a input age,height,name and display them with data type 
+
+name = input("Enter Your Name :")
+age = int(input("Enter Your Age :"))
+height = float(input("Enter Your Height :"))
+
+print(type(name))
+print(type(age))
+print(type(height))
