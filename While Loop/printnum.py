9cc820e5efc14b87c7b8e441fbc133db23c5aec7
Gauravdev28 <gauravagldeveloper28@gmail.com>
2026-06-08 17:23:29 +0530

    Printing Number from 1 to 10

--- While Loop/printnum.py
+++ While Loop/printnum.py
@@ -1,8 +1,6 @@
 # Printing 1 to 100 while using while loop
 
-number = 1
-while number <=100 :
-    print(number)
-    number +=1
-
-print("End Loop")
+num = int(input("Enter Number"))
+while num <=10 :
+    print(num)
+    num+=1
