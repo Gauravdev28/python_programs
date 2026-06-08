86c3fe65e1e7dc5190846dec1badcb66aae9a00b
Gauravdev28 <gauravagldeveloper28@gmail.com>
2026-06-08 17:41:53 +0530

    While Loop Question

---
+++ While Loop/even.py
@@ -0,0 +1,6 @@
+# WAP where we print even number 
+
+num = 0
+while num<=20 :
+    print(num)
+    num +=2
---
+++ While Loop/sum_num.py
@@ -0,0 +1,8 @@
+# WAP where it sum the number of user input 
+
+num = 0
+sum = 0
+while num<=10 :
+    sum+=num
+    num+=1
+print(sum)
