bab3a97b16b3270f69f6be6a66488334c4ae7b72
Gauravdev28 <gauravagldeveloper28@gmail.com>
2026-06-12 21:14:45 +0530

    Printing Number

--- Recursion/printnum.py
+++ Recursion/printnum.py
@@ -1,20 +1,11 @@
-# Print Number from 1 to n 
-
-def num_print(n):
-    if(n==0) :
-        return 
-    num_print(n-1)
-    print(n)
-num_print(5)
+# WAP by using Recurssion to print number from 0 to user input 
 
 
-# Print number from n to 1 
-
-def reverse_num(n):
+def print_num(n) :
     if (n==0) :
-        return
+        return 
     
+    print_num(n-1)
     print(n)
-    reverse_num(n-1)
 
-reverse_num(5)
+print_num(5)
